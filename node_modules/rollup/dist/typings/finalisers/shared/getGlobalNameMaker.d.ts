cb050841a83636eb5adb85cbfc272e32f7633856
gcofficial <gurievcreative@gmail.com>
2018-08-08 08:19:15 +0300

    Remove node modules

--- node_modules/rollup/dist/typings/finalisers/shared/getGlobalNameMaker.d.ts
+++
@@ -1,4 +0,0 @@
-import { GlobalsOption } from '../../rollup/index';
-import Graph from '../../Graph';
-import { ModuleDeclarationDependency } from '../../Chunk';
-export default function getGlobalNameMaker(globals: GlobalsOption, graph: Graph, fallback?: string): (dependency: ModuleDeclarationDependency) => string;
